3eabd36d6eb422c779157f0f3546f66113fdce1d
Harmony Burke <theharmonyburke@gmail.com>
2023-12-10 14:29:35 -0600

    started adding functions for all options

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db=mysql.createConnection(
     },
     console.log(`Connected to company_db.`)
 );
-
+function start(){
 inquirer
     .prompt([
         {
@@ -28,7 +28,82 @@ inquirer
             choices: ['Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
         },
     ])
+    // .then(function(choice) => {
+    //     switch(choice.action) {
+    //         case "View All Departments":
+    //             viewDepartment();
+    //             break;
+    //             case "View All Roles":
+    //                 viewRoles();
+    //                 break;
+    //                 case "View All Employees":
+    //                     viewEmployees();
+    //                     break;
+    //     }
+    // }) 
     
-    .then((action) =>{
-        
-    }) 
+}
+    function viewDepartment() {
+       let query= "SELECT * FROM department";
+    //    selects everything from the table department
+    db.query(query, function(err,res){
+        if (err) throw err;
+        console.table(res);
+        // shows the response
+    })
+    }
+
+    function viewRoles() {
+        let query= 'SELECT * FROM roles';
+        // select all from roles table 
+        db.query(query, function(err,res){
+            if (err) throw err;
+            console.table(res);
+            // show the response
+        })
+    }
+    function viewEmployees(){
+        let query='SELECT * FROM employee';
+        // select all from employee
+        db.query(query, function(err,res){
+            if (err) throw err;
+            console.table(res);
+        })
+    }
+    function addDepartment(){
+        inquirer.prompt([
+            {
+                type:'input',
+                message:'What is the name of the department?'
+                name:'departmentName'
+            }
+        ])
+    }
+    function addRole(){
+        inquirer.prompt([
+            {
+                type:'input',
+                message:'What is the name of the role?',
+                name:'roleName'
+            },
+            {
+                type:'input',
+                message:'What is the salary for this role',
+                name:'newSalary'
+            },
+            {
+                type:'list',
+                message:'Which department does this role belong to?'
+                choices:["Accounting",
+                "Marketing",
+                "Sales",
+                "Development",
+                "Human Resources"
+                ],
+                name:'roleDept'
+            }
+        ])
+        .then(function(answer){
+            db.query("INSERT INTO roles (title, salary, department_id")
+        })
+    }
